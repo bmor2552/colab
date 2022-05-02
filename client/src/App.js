694e1ee7d7f6cdef377fcedd14b83a5091f23232
GunnySensei <gunnerwagoner@hotmail.com>
2022-05-02 14:10:06 -0500

    added templates/gallery component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Gallery from "./components/Gallery";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">ST Aesthetics</header>
+      <Gallery />
     </div>
   );
 }
